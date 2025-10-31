5ba9835ba9f48a7bfc59fa8950217bcc7ef5a01b
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2025-10-31 16:57:37 +0100

    Update coupled line coupler configurations and add new lumped coupled line couplers
    - Changed the DDS file name for the open history to point to the new lumped coupled line couplers.
    - Updated the de_sim configuration to include new schematic files for lumped coupled line couplers.
    - Added a new DDS file for lumped coupled line couplers with detailed plot configurations and traces.
    - Modified the netlist log to reflect the new top design for lumped coupled line couplers.
    - Created a new workspace file to include the new lumped coupled line couplers and associated configurations.

---
+++ workspace.ads
@@ -0,0 +1,40 @@
+<ADSWorkspace Revision="3" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Log Name="search_history.log" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="ads_serdes_ref_channel" />
+        <Library Name="ads_quantum" />
+        <Library Name="coupled-line-coupler_lib" />
+        <Cell Name="coupled-line-coupler_lib:ideal_coupled_line_couplers" />
+        <Log Name="netlist.log" />
+        <Dataset Name="ideal_coupled_line_couplers.ds" />
+        <Data_Files Name="ideal_coupled_line_couplers_data/logFile.txt" />
+        <Data_Files Name="ideal_coupled_line_couplers.ds" />
+        <ConfigFile Name="linecalc.cfg" />
+        <ConfigFile Name="dds.cfg" />
+        <Data_Display Name="ideal_coupled_line_couplers.dds" />
+        <Cell Name="coupled-line-coupler_lib:lumped_coupled_line_couplers" />
+    </Workspace>
+</ADSWorkspace>
